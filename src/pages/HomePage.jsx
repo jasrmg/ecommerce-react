69cb1aa4760b4e5e7ef7cd7e7bf353373b1a5178
jeff <alainnnsarmago@gmail.com>
2025-09-26 13:04:17 +0800

    feat: used axios to fetch data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 import { Header } from "../components/Header";
 import { products } from "../../starting-code/data/products.js";
 import "./HomePage.css";
@@ -5,6 +7,10 @@ import "./HomePage.css";
 import checkmark from "../assets/images/icons/checkmark.png";
 
 export const HomePage = () => {
+  axios.get("http://localhost:3000/api/products").then((response) => {
+    console.log(response.data);
+  });
+
   return (
     <>
       <title>Ecommerce Project</title>
