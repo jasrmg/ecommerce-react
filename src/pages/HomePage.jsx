e1cac940de34f1f8324c5bceca90fa13bc3b8713
jeff <alainnnsarmago@gmail.com>
2025-09-26 10:48:07 +0800

    feat: solved 6x. challenge exercises

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ export const HomePage = () => {
   return (
     <>
       <title>Ecommerce Project</title>
+      <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
       <Header />
 
       <div className="home-page">
