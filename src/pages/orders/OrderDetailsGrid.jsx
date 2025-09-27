07ff89b2c4def555c6e0af298bd8ec835c934769
jeff <alainnnsarmago@gmail.com>
2025-09-27 10:31:37 +0800

    feat: c7i

--- src/pages/orders/OrderDetailsGrid.jsx
+++ src/pages/orders/OrderDetailsGrid.jsx
@@ -29,7 +29,7 @@ export const OrderDetailsGrid = ({ order }) => {
             </div>
 
             <div className="product-actions">
-              <Link to="/tracking">
+              <Link to={`/tracking/${order.id}/${product.productId}`}>
                 <button className="track-package-button button-secondary">
                   Track package
                 </button>
