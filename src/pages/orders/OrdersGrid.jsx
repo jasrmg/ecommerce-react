53a8543bb7246983558db1dd5a4f3a6f6ee9cb3b
jeff <alainnnsarmago@gmail.com>
2025-09-27 10:22:46 +0800

    feat: 7f

--- src/pages/orders/OrdersGrid.jsx
+++ src/pages/orders/OrdersGrid.jsx
@@ -1,8 +1,8 @@
 import { Fragment } from "react";
 import { Link } from "react-router";
-import dayjs from "dayjs";
-import { formatMoney } from "../../utils/money";
-import buyAgain from "../../assets/images/icons/buy-again.png";
+
+import { OrderHeader } from "./OrderHeader";
+import { OrderDetailsGrid } from "./OrderDetailsGrid";
 
 export const OrdersGrid = ({ orders }) => {
   return (
@@ -10,60 +10,9 @@ export const OrdersGrid = ({ orders }) => {
       {orders.map((order) => {
         return (
           <div key={order.id} className="order-container">
-            <div className="order-header">
-              <div className="order-header-left-section">
-                <div className="order-date">
-                  <div className="order-header-label">Order Placed:</div>
-                  <div>{dayjs(order.orderTimeMs).format("MMMM D")}</div>
-                </div>
-                <div className="order-total">
-                  <div className="order-header-label">Total:</div>
-                  <div>{formatMoney(order.totalCostCents)}</div>
-                </div>
-              </div>
+            <OrderHeader order={order} />
 
-              <div className="order-header-right-section">
-                <div className="order-header-label">Order ID:</div>
-                <div>{order.id}</div>
-              </div>
-            </div>
-
-            <div className="order-details-grid">
-              {order.products.map((product) => {
-                return (
-                  <Fragment key={product.productId}>
-                    <div className="product-image-container">
-                      <img src={product.product.image} />
-                    </div>
-
-                    <div className="product-details">
-                      <div className="product-name">{product.product.name}</div>
-                      <div className="product-delivery-date">
-                        Arriving on:{" "}
-                        {dayjs(product.estimatedDeliveryTimeMs).format(
-                          "MMMM D"
-                        )}
-                      </div>
-                      <div className="product-quantity">
-                        Quantity: {product.quantity}
-                      </div>
-                      <button className="buy-again-button button-primary">
-                        <img className="buy-again-icon" src={buyAgain} />
-                        <span className="buy-again-message">Add to Cart</span>
-                      </button>
-                    </div>
-
-                    <div className="product-actions">
-                      <Link to="/tracking">
-                        <button className="track-package-button button-secondary">
-                          Track package
-                        </button>
-                      </Link>
-                    </div>
-                  </Fragment>
-                );
-              })}
-            </div>
+            <OrderDetailsGrid order={order} />
           </div>
         );
       })}
