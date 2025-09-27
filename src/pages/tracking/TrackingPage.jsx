b760c1d863b78af6434e6551d7f033c155bb361f
jeff <alainnnsarmago@gmail.com>
2025-09-27 11:06:11 +0800

    feat: c7l

--- src/pages/tracking/TrackingPage.jsx
+++ src/pages/tracking/TrackingPage.jsx
@@ -26,38 +26,51 @@ export const TrackingPage = ({ cart }) => {
   const orderProduct = order.products.find((orderProduct) => {
     return orderProduct.productId === productId;
   });
+
+  const totalDeliveryTimeMs =
+    orderProduct.estimateDeliveryTimeMs - order.orderTimeMs;
+  const timePassedMs = dayjs().valueOf() - order.orderTimeMs;
+  let deliveryPercent = (timePassedMs / totalDeliveryTimeMs) * 100;
+  if (deliveryPercent > 100) {
+    deliveryPercent = 100;
+  }
   return (
     <>
       <link rel="icon" type="image/svg+xml" href="tracking-favicon.png" />
       <title>Tracking</title>
 
-      <Header />
+      <Header cart={cart} />
 
-      <div class="tracking-page">
-        <div class="order-tracking">
-          <Link class="back-to-orders-link link-primary" to="/orders">
+      <div className="tracking-page">
+        <div className="order-tracking">
+          <Link className="back-to-orders-link link-primary" to="/orders">
             View all orders
           </Link>
 
-          <div class="delivery-date">
-            Arriving on{" "}
+          <div className="delivery-date">
+            Arriving on
             {dayjs(orderProduct.estimateDeliveryTimeMs).format("dddd, MMMM D")}
           </div>
 
-          <div class="product-info">{orderProduct.product.name}</div>
+          <div className="product-info">{orderProduct.product.name}</div>
 
-          <div class="product-info">Quantity: {orderProduct.quantity}</div>
+          <div className="product-info">Quantity: {orderProduct.quantity}</div>
 
-          <img class="product-image" src={orderProduct.product.image} />
+          <img className="product-image" src={orderProduct.product.image} />
 
-          <div class="progress-labels-container">
-            <div class="progress-label">Preparing</div>
-            <div class="progress-label current-status">Shipped</div>
-            <div class="progress-label">Delivered</div>
+          <div className="progress-labels-container">
+            <div className="progress-label">Preparing</div>
+            <div className="progress-label current-status">Shipped</div>
+            <div className="progress-label">Delivered</div>
           </div>
 
-          <div class="progress-bar-container">
-            <div class="progress-bar"></div>
+          <div className="progress-bar-container">
+            <div
+              className="progress-bar"
+              style={{
+                width: `${deliveryPercent}%`,
+              }}
+            ></div>
           </div>
         </div>
       </div>
